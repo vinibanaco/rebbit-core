37e2b9c084436c31ad15c7e2c3045970b5d6294d
Vinícius Marrara Banaco <vinibanaco@gmail.com>
2025-04-27 18:59:29 -0300

    chore: add configs for module federation

--- src/pages/post/index.tsx
+++ src/pages/post/index.tsx
@@ -1,4 +1,9 @@
+import { lazy, Suspense } from "react";
 import Page from "../../ui-components/page";
+const Comments = lazy(
+  // @ts-expect-error - micro-frontend
+  async () => import("comments/comments")
+);
 
 import styles from "./index.module.css";
 
@@ -31,20 +36,9 @@ export default function PostPage() {
           </div>
 
           <section className={styles.comments}>
-            <h2>Comments</h2>
-            <div className={styles.commentInput}>
-              <textarea placeholder="What are your thoughts?" />
-              <button>Comment</button>
-            </div>
-            <div className={styles.comment}>
-              <p className={styles.commentMeta}>u/commenter • 1h ago</p>
-              <p className={styles.commentText}>This is a comment.</p>
-              <div className={styles.commentActions}>
-                <button>Upvote</button>
-                <button>Downvote</button>
-                <button>Reply</button>
-              </div>
-            </div>
+            <Suspense fallback={<span>Loading comments...</span>}>
+              <Comments />
+            </Suspense>
           </section>
         </main>
 
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,30 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { federation } from "@module-federation/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  server: {
+    port: 5173,
+  },
+  plugins: [
+    react(),
+    federation({
+      name: "core",
+      remotes: {
+        comments: {
+          type: "module",
+          name: "comments",
+          entry: "http://localhost:5174/remoteEntry.js",
+          entryGlobalName: "comments",
+          shareScope: "default",
+        },
+      },
+      filename: "remoteEntry.js",
+      shared: ["react", "react-dom"],
+    }),
+  ],
+  build: {
+    target: "chrome89",
+  },
+});
